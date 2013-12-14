96b2bda0bf95ebd352833c73f57adf0e50a07f9c
Tomasz Ostrowski <ostrowski_tomasz@yahoo.com>
2013-12-14 14:10:14 +0100

    argument check for InputValidator added.

--- src/main/java/plan3/recruitment/backend/validators/InputValidator.java
+++ src/main/java/plan3/recruitment/backend/validators/InputValidator.java
@@ -2,11 +2,17 @@ package plan3.recruitment.backend.validators;
 
 import org.hibernate.validator.internal.constraintvalidators.EmailValidator;
 
+import static org.apache.commons.lang.StringUtils.isBlank;
+
 public class InputValidator {
 
     private final EmailValidator emailValidator = new EmailValidator();
 
     public boolean isEmailValid(String email) {
+        if (isBlank(email)) {
+            return false;
+        }
+
         return emailValidator.isValid(email, null);
     }
 }
--- src/test/java/plan3/recruitment/backend/validators/InputValidatorTest.java
+++ src/test/java/plan3/recruitment/backend/validators/InputValidatorTest.java
@@ -1,16 +1,20 @@
 package plan3.recruitment.backend.validators;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(JUnitParamsRunner.class)
 public class InputValidatorTest {
 
     private InputValidator inputValidator = new InputValidator();
 
     @Test
-    public void shouldProcessValidEmail() {
+    public void shouldReturnTrueForValidEmail() {
         // given
         String validEmail = "qiuck@brown.fox";
 
@@ -22,9 +26,21 @@ public class InputValidatorTest {
     }
 
     @Test
-    public void shouldThrowExceptionForInvalidEmail() {
+    public void shouldReturnFalseForNull() {
         // given
-        String invalidEmail = "sorry@";
+        String nullEmail = null;
+
+        // when
+        boolean result = inputValidator.isEmailValid(nullEmail);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    @Parameters({"whatever", "whatever@", "whatever@@gmail.com", "@whatever" , "", "  "})
+    public void shouldReturnFalseForInvalidEmail(String invalidEmail) {
+        // given each invalid email from @Parameters list
 
         // when
         boolean result = inputValidator.isEmailValid(invalidEmail);
